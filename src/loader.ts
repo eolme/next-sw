5bf2b17df976140a2236c6a3e587cd38d6aa55fc
eolme <im@petrov.engineer>
2022-07-02 14:38:18 +0300

    Add some swc optimizations

--- src/loader.ts
+++ src/loader.ts
@@ -15,13 +15,31 @@ export default function ServiceWorkerLoader(
     sourceMaps: false,
     isModule: true,
     minify: options.minify,
+    module: {
+      type: 'es6',
+      lazy: true
+    },
     jsc: {
       parser: {
         syntax: 'typescript'
       },
       target: 'es2015',
       loose: true,
-      externalHelpers: false
+      externalHelpers: false,
+      transform: {
+        optimizer: {
+          globals: {
+            vars: {
+              'globalThis': 'self',
+              'process.env.NODE_ENV': options.minify ? '"production"' : '"development"'
+            },
+            typeofs: {
+              window: 'undefined',
+              document: 'undefined'
+            }
+          }
+        }
+      }
     }
   }).code.toString();
 }
